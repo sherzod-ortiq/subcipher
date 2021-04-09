a61b9c3961747e0ae3aa598aac62be043e401d9f
Sherzodbek Ortikjonov (Akvelon INC) <v-sortik@microsoft.com>
2021-04-09 22:24:33 +0300

    Basic file read/write

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,30 +10,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Subcipher',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Subcipher'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  final String title;
+
   MyHomePage({this.title = ""});
 
-  final String title;
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path.toString();
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/chars.txt');
+  }
+
+  Future<File> writeChars(String chars) async {
+    final file = await _localFile;
+    return file.writeAsString(chars);
+  }
+
+  Future<String> readChars() async {
+    try {
+      final file = await _localFile;
+      String contents = await file.readAsString();
+
+      return contents;
+    } catch (e) {
+      return 'Error';
+    }
+  }
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _counter = '';
 
-  void _incrementCounter() {
+  Future<void> _incrementCounter() async {
+    //final String path = await widget._localPath;
+    widget.writeChars("Hello, Sher!");
+    final String fileContent = await widget.readChars();
     setState(() {
-      _counter++;
+      _counter = fileContent;
     });
   }
 
